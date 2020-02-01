7599806fc4478f2a4b244b83c112e87b27f6d6bd
kareemelkasaby1 <kareem.elkasaby1@gmail.com>
2020-02-01 17:03:56 +0200

    finished Display all

--- displayAll.sh
+++ displayAll.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-awk -F: 'BEGIN{}{if(NR!=1){ printf("%s",$1);for(j=0;j<((7-length($1)));j++){printf(" ")};printf("|");  }}END{printf("\n")}' ${myDatabasePath}/.$1.md
-awk -F: 'BEGIN{}{if(NR!=1){ for(i=0;i<NF;i++){ printf "________"}; print "" ; for(i=1;i<=NF;i++){ printf("%s",$i);for(j=0;j<((7-length($i)));j++){printf(" ")};printf("|")};printf("\n")  }}' ${myDatabasePath}/$1
+maxlen=$(awk -F: 'BEGIN{maxlen=0} {if(NR!=1) {for(j=1;j<=NF;j++) {if(maxlen<length($j)) {maxlen=length($j)} } } } END{print maxlen}' ${myDatabasePath}/$1)
+maxlen=$(($maxlen+3))
+awk -v maxlen=$(($maxlen)) -F: 'BEGIN{}{} END{for(i=0;i<((((maxlen)-length($i)))*NF);i++){ printf "_"};printf "\n"}' ${myDatabasePath}/$1
+awk -v maxlen=$(($maxlen)) -F:  'BEGIN{}{if(NR!=1){ printf("%s",$1);for(j=0;j<((maxlen-length($1)));j++){printf(" ")};printf("|");  }}END{printf("\n")}' ${myDatabasePath}/.$1.md
+awk -v maxlen=$(($maxlen)) -F: 'BEGIN{}{if(NR!=1){ for(i=0;i<((((maxlen)-length($i)))*NF);i++){ printf "_"}; print "" ; for(i=1;i<=NF;i++){ printf("%s",$i);for(j=0;j<((maxlen-length($i)));j++){printf(" ")};printf("|")};printf("\n")}} END{for(i=0;i<((((maxlen)-length($i)))*NF);i++){ printf "_"};printf "\n";printf "\n"}' ${myDatabasePath}/$1
