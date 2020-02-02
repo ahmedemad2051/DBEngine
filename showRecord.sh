753d7e43a17ae98de7d91586f553cfd8d5dd1698
kareemelkasaby1 <kareem.elkasaby1@gmail.com>
2020-02-02 23:49:22 +0200

    display all content before print specialy 1 row

--- showRecord.sh
+++ showRecord.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 function showRecordFromTable {
+    source displayAll.sh ${1}
     while true
     do
         echo "${yellow}do you want proceed y or n ${reset}"
@@ -18,9 +19,7 @@ function showRecordFromTable {
                 feildToBeEditedColumnNumber=$(awk -v field="$feildName" -F: '{if($1 == field) print NR}' ${myDatabasePath}/".${1}.md");
                 feildToBeEditedColumnNumber=$(($feildToBeEditedColumnNumber - 1))
                 sed -n "/$primaryKey/p" ${myDatabasePath}/$1 >> ${myDatabasePath}/".${1}.temp"
-                cat ${myDatabasePath}/".${1}.temp"
                 maxlen=$(awk -F: 'BEGIN{maxlen=0} {for(j=1;j<=NF;j++) {if(maxlen < length($j)) {maxlen = length($j)}  } } END{print maxlen+4}' ${myDatabasePath}/".${1}.temp")
-                echo $maxlen
                 awk -v maxleng=$(($maxlen)) -F: 'BEGIN{}{} END{for(i=0;i<((maxleng*NF)+4);i++){ printf "_"};printf "\n"}' ${myDatabasePath}/".${1}.temp"
                 awk -v maxleng=$(($maxlen)) -F: 'BEGIN{}{if(NR!=1){ printf("%s",$1);for(j=0;j<((maxleng-length($1)));j++){printf(" ")};printf("|");  }}END{printf("\n")}' ${myDatabasePath}/.$1.md
                 awk -v maxleng=$(($maxlen)) -F: 'BEGIN{}{for(i=0;i<((maxleng*NF)+4);i++){ printf "_"}; print "" ; for(i=1;i<=NF;i++){ printf("%s",$i);for(j=0;j<((maxleng-length($i)));j++){printf(" ")};printf("|")};printf("\n")} END{for(i=0;i<((maxleng*NF)+4);i++){ printf "_"};printf "\n";printf "\n"}' ${myDatabasePath}/".${1}.temp"
